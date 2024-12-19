741da25813f3830da32dfa83191e423ec0b9836b
Qy742698 <155599655+Qy742698@users.noreply.github.com>
2024-12-19 19:59:06 +0800

    chore: add README.OpenSource and OAT.XML and COMMITTERS.md (#9)

--- harmony/mixpanel/src/main/ets/mixpanelJs/mixpanel-core.js
+++ harmony/mixpanel/src/main/ets/mixpanelJs/mixpanel-core.js
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd. All rights reserved
+ * Use of this source code is governed by a MIT license that can be
+ * found in the LICENSE file.
+ */
+
 import { MixpanelQueueManager } from "./mixpanel-queue";
 import { MixpanelNetwork } from "./mixpanel-network";
 import { SessionMetadata } from "./mixpanel-utils";
